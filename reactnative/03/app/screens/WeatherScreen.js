2c2e89fc504dbb76da1b3b5a4700b053312967ff
loitd <loitranduc@gmail.com>
2019-06-22 23:49:30 +0700

    x

--- reactnative/03/app/screens/WeatherScreen.js
+++ reactnative/03/app/screens/WeatherScreen.js
@@ -1,9 +1,14 @@
+// --------------------------------------
+// React Native Lesson 04
+// youtube.com/tranducloi
+// --------------------------------------
 import React from 'react';
 import {View, Text, Image, 
     StyleSheet, TextInput, 
     KeyboardAvoidingView,
     ImageBackground, Platform
-} from 'react-native'
+} from 'react-native';
+import SearchInput from '../components/SearchInput'
 
 export default class WeatherScreen extends React.Component{
     // define static methods
@@ -13,6 +18,9 @@ export default class WeatherScreen extends React.Component{
     
     // begin render
     render() {
+        // default location
+        const location = 'Hà Nội';
+
         return (
             <KeyboardAvoidingView style={mstyles.container} behavior="padding">       
             {/* <View style={mstyles.container}> */}
@@ -21,14 +29,12 @@ export default class WeatherScreen extends React.Component{
                 style={mstyles.imgcontainer}
                 imageStyle={mstyles.img}>
                     <View style={mstyles.content}>
-                        <Text style={[mstyles.h1, mstyles.txt]}>Hà Nội</Text>
+                        <Text style={[mstyles.h1, mstyles.txt]}>{location}</Text>
                         <Text style={[mstyles.small, mstyles.txt]}>Sunny</Text>
                         <Text style={[mstyles.h2, mstyles.txt]}>23°C</Text>
                         
-                        <TextInput placeholder="Tìm kiếm địa danh"
-                        placeholderTextColor="black"
-                        clearButtonMode="always"
-                        style={mstyles.textinp}/>
+                        <SearchInput placeholder="Tìm kiếm địa danh"
+                        placeholderTextColor="black"/>
                         {/* The way we call image in React Native */}
                         {/* <Image source={require('../assets/images/background.jpg')} style={mstyles.backgroundimg} /> */}
 
@@ -73,6 +79,7 @@ const mstyles = StyleSheet.create({
         fontSize: 44,
         lineHeight: 50,
         color: 'yellow',
+        fontWeight: 'bold',
     },
     h2: {
         fontSize: 38,
@@ -84,16 +91,4 @@ const mstyles = StyleSheet.create({
         lineHeight: 22,
         color: 'yellow',
     },
-    textinp: {
-        backgroundColor: '#FBBC04',
-        height: 40,
-        width: 300,
-        alignSelf: 'center',
-        paddingHorizontal: 10,
-        marginHorizontal: 20,
-        marginTop: 200,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    
 });
